a33ee5f541bd1155f1621940a25be119b49439e5
Garturo123 <gaat1806@gmail.com>
2025-05-09 14:42:05 -0600

    Palabras Alreves y palidromos

---
+++ Laboratorio1/src/EstControl.java
@@ -0,0 +1,13 @@
+/**
+ *
+ * @author gaat1
+ */
+public class EstControl {
+    public static void main(String[]args){
+    int cont = 1;
+    while(cont<=5){
+        System.out.println("No."+cont+" Hola Mundo!");
+        cont++;
+    }
+    }
+}
